f22ebccf29a8aea073159b6837b0024cef42b584
Nelo Mitranim <me@mitranim.com>
2023-07-27 16:11:50 +0200

    utility for generating OpenAPI schema for function inputs

---
+++ go/cmd_print_oas_doc.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"_/go/u"
+	"fmt"
+
+	"github.com/mitranim/cmd"
+	"github.com/mitranim/gg"
+	"github.com/mitranim/gg/grepr"
+	"github.com/mitranim/oas"
+)
+
+type CmdPrintOasDoc[_ any] struct {
+	OutPath string `flag:"--out-path" desc:"path for output file"`
+}
+
+func (self CmdPrintOasDoc[_]) RunCli() {
+	gg.FlagParse(cmd.Args(), &self)
+	self.Run()
+}
+
+func (self CmdPrintOasDoc[A]) Run() {
+	typ := gg.Type[A]()
+
+	var doc oas.Doc
+	doc.TypeSchema(typ)
+
+	src := doc
+
+	if gg.IsZero(self.OutPath) {
+		grepr.Println(src)
+		fmt.Println(u.JsonPretty(gg.JsonString(src)))
+	} else {
+		u.PolyEncodeFileOpt(self.OutPath, src)
+	}
+}
--- go/main.go
+++ go/main.go
@@ -19,5 +19,11 @@ func main() {
 	cmd.Map{
 		`oai_conv_file`: CmdOaiConvFile{ApiKey: key}.RunCli,
 		`oai_conv_dir`:  CmdOaiConvDir{ApiKey: key}.RunCli,
+
+		// May be helpful for generating JSON schemas for request templates.
+		// Substitute the input type as needed.
+		`print_oas_doc`: CmdPrintOasDoc[FunctionWriteFilesInput]{
+			OutPath: `local/oas_doc.yaml`,
+		}.RunCli,
 	}.Get()()
 }
