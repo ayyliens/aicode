9b80965242dad04967a4e5995a824e32ecd1abb1
Yury Egorenkov <yury.egorenkov@gmail.com>
2023-11-22 11:55:08 +0100

    comment

--- go/oai/oai_chat_completion_request.go
+++ go/oai/oai_chat_completion_request.go
@@ -9,6 +9,7 @@ import (
 var OpenAiDefaultModel = `gpt-3.5-turbo-16k`
 
 type ChatCompletionRequest struct {
+	// `Model` can be overridden on a request when `HttpClient.ApiModel` is not zero.
 	Model            string                  `json:"model,omitempty"             yaml:"model,omitempty"             toml:"model,omitempty"`
 	Messages         []ChatCompletionMessage `json:"messages,omitempty"          yaml:"messages,omitempty"          toml:"messages,omitempty"`
 	MaxTokens        uint64                  `json:"max_tokens,omitempty"        yaml:"max_tokens,omitempty"        toml:"max_tokens,omitempty"`
