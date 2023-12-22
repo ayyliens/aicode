58a691c66b0fa4948cec254c8df5ee15d4610d0c
Mike Frolov <mike@frolov.dev>
2023-12-22 22:58:36 +1100

    fix for generated prompt file name

--- go/u/u_prompt.go
+++ go/u/u_prompt.go
@@ -1,14 +1,15 @@
 package u
 
 import (
+	"_/go/oai"
 	"path/filepath"
 
 	"github.com/mitranim/gg"
 )
 
 type Prompt struct {
-	Role string `json:"role" desc:"role (system, function, user)"`
-	Body string `json:"body" desc:"file content"`
+	Role oai.ChatMessageRole `json:"role" desc:"role (system, function, user)"`
+	Body string              `json:"body" desc:"file content"`
 }
 
 func (self Prompt) Validate() {
@@ -20,5 +21,11 @@ func (self Prompt) Validate() {
 func (self Prompt) WriteTo(ver Version, out string) {
 	self.Validate()
 	// TODO support yaml
-	WriteFileRec(filepath.Join(out, ver.String()+`_`+self.Role+`_message.md`), self.Body)
+	fileName := oai.VersionedFileName{
+		Version: ver,
+		Role:    self.Role,
+		Type:    oai.VersionedFileTypeMessage,
+		Ext:     oai.ChatCompletionMessageDefaultExtForText,
+	}.String()
+	WriteFileRec(filepath.Join(out, fileName), self.Body)
 }
